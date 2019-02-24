265de4f2572ce4f73eff95e2767096d087a93a10
varhb <Balazs.Varhegyi@pega.com>
2019-02-24 20:14:53 +0100

    Find vertical seam

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Picture picture = new Picture(new File("resources/5x6.png"));
         SeamCarver seamCarver = new SeamCarver(picture);
-        int[] verticalSeam = seamCarver.findVerticalSeam();
+        int[] verticalSeam = seamCarver.findHorizontalSeam();
         for (int i : verticalSeam) {
             System.out.println(i);
         }
--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -1,24 +1,29 @@
 import edu.princeton.cs.algs4.Picture;
-import edu.princeton.cs.algs4.Point2D;
-
-import java.text.DecimalFormat;
 
 public class SeamCarver {
 
-    private static DecimalFormat df2 = new DecimalFormat(".##");
-    private final Picture picture;
-    int[][] energy;
+    private int[][] pictureArray;
+    private int width;
+    private int height;
+    private int[][] energy;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         if (picture == null) {
             throw new IllegalArgumentException();
         }
-        this.picture = new Picture(picture);
+        this.width = picture.width();
+        this.height = picture.height();
+        this.pictureArray = new int[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                pictureArray[x][y] = picture.getRGB(x,y);
+            }
+        }
         this.energy = new int[width()][height()];
 
         fillEnergyMatrix();
-        printEnergyMatrix();
+//        printEnergyMatrix();
     }
 
     private void printEnergyMatrix() {
@@ -43,12 +48,12 @@ public class SeamCarver {
     }
 
     private int energyInt(int x, int y) {
-        int leftRgb = picture.getRGB(x-1, y);
-        int rightRgb = picture.getRGB(x+1, y);
+        int leftRgb = pictureArray[x-1][y];
+        int rightRgb = pictureArray[x+1][y];
         int energyX = getEnergy(leftRgb, rightRgb);
 
-        int aboveRgb = picture.getRGB(x, y - 1);
-        int belowRgb = picture.getRGB(x, y + 1);
+        int aboveRgb = pictureArray[x][y - 1];
+        int belowRgb = pictureArray[x][y + 1];
         int energyY = getEnergy(aboveRgb, belowRgb);
 
         return energyX + energyY;
@@ -73,17 +78,23 @@ public class SeamCarver {
 
     // current picture
     public Picture picture() {
-        return new Picture(picture);
+        Picture picture = new Picture(width,height);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                picture.setRGB(x,y,pictureArray[x][y]);
+            }
+        }
+        return picture;
     }
 
     // width of current picture
     public int width() {
-        return picture.width();
+        return width;
     }
 
     // height of current picture
     public int height() {
-        return picture.height();
+        return height;
     }
 
     // energy of pixel at column x and row y
@@ -92,6 +103,77 @@ public class SeamCarver {
             throw new IllegalArgumentException();
         }
         return Math.sqrt(energy[x][y]);
+    }
+
+    // sequence of indices for horizontal seam
+    //TODO: transpose picture and reuse
+    public int[] findHorizontalSeam() {
+        return shortestPath(transpose(this.energy));
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return shortestPath(this.energy);
+    }
+
+    private int[][] transpose(int[][] matrix) {
+        int[][] result = new int[matrix[0].length][matrix.length];
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[0].length; y++) {
+                result[y][x] = matrix[x][y];
+            }
+        }
+        return result;
+    }
+
+    private int[] shortestPath(int[][] energy) {
+        int width = energy.length;
+        int height = energy[0].length;
+        double[][] distTo = new double[width][height];
+        int[][] edgeTo = new int[width][height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (y == 0) {
+                    distTo[x][y] = energy[x][y];
+                    edgeTo[x][y] = getId(x,y);
+                } else {
+                    double aboveDist = distTo[x][y-1] + energy[x][y];
+                    distTo[x][y] = aboveDist;
+                    edgeTo[x][y] = getId(x, y-1);
+                    double aboveLeftDist = x > 0 ? distTo[x-1][y-1] + energy[x][y] : Double.MAX_VALUE;
+                    if (aboveLeftDist < distTo[x][y]) {
+                        distTo[x][y] = aboveLeftDist;
+                        edgeTo[x][y] = getId(x-1, y-1);
+                    }
+                    double aboveRightDist = x < width() - 2 ? distTo[x+1][y-1] + energy[x][y] : Double.MAX_VALUE;
+                    if (aboveRightDist < distTo[x][y]) {
+                        distTo[x][y] = aboveRightDist;
+                        edgeTo[x][y] = getId(x+1, y-1);
+                    }
+//                    System.out.print("("+getX(edgeTo[x][y]) +","+ df2.format(distTo[x][y])+")");
+                }
+
+            }
+//            System.out.println();
+        }
+        double shortestPath = Double.MAX_VALUE;
+        Integer shortestDestId = null;
+        for (int x = 0; x < width; x++) {
+            if (distTo[x][height-1] < shortestPath) {
+                shortestPath = distTo[x][height-1];
+                shortestDestId = getId(x, height-1);
+            }
+        }
+
+        int[] result = new int[height];
+        int lastId = shortestDestId;
+        for (int i = height-1; i > -1 ; i--) {
+            int x = getX(lastId);
+            int y = getY(lastId);
+            result[i] = x;
+            lastId = edgeTo[x][y];
+        }
+        return result;
     }
 
     private int getId(int x, int y) {
@@ -104,61 +186,6 @@ public class SeamCarver {
 
     private int getY(int id) {
         return id / width();
-    }
-
-    // sequence of indices for horizontal seam
-    public int[] findHorizontalSeam() {
-        return null;
-    }
-
-
-    // sequence of indices for vertical seam
-    public int[] findVerticalSeam() {
-        double[][] distTo = new double[width()][height()];
-        int[][] edgeTo = new int[width()][height()];
-        for (int y = 0; y < height(); y++) {
-            for (int x = 0; x < width(); x++) {
-                if (y == 0) {
-                    distTo[x][y] = energy(x,y);
-                    edgeTo[x][y] = getId(x,y);
-                } else {
-                    double aboveDist = distTo[x][y-1] + energy(x,y);
-                    distTo[x][y] = aboveDist;
-                    edgeTo[x][y] = getId(x, y-1);
-                    double aboveLeftDist = x > 0 ? distTo[x-1][y-1] + energy(x,y) : Double.MAX_VALUE;
-                    if (aboveLeftDist < distTo[x][y]) {
-                        distTo[x][y] = aboveLeftDist;
-                        edgeTo[x][y] = getId(x-1, y-1);
-                    }
-                    double aboveRightDist = x < width() - 2 ? distTo[x+1][y-1] + energy(x,y) : Double.MAX_VALUE;
-                    if (aboveRightDist < distTo[x][y]) {
-                        distTo[x][y] = aboveRightDist;
-                        edgeTo[x][y] = getId(x+1, y-1);
-                    }
-                    System.out.print("("+getX(edgeTo[x][y]) +","+ df2.format(distTo[x][y])+")");
-                }
-
-            }
-            System.out.println();
-        }
-        double shortestPath = Double.MAX_VALUE;
-        Integer shortestDestId = null;
-        for (int x = 0; x < width(); x++) {
-            if (distTo[x][height()-1] < shortestPath) {
-                shortestPath = distTo[x][height()-1];
-                shortestDestId = getId(x, height()-1);
-            }
-        }
-
-        int[] result = new int[height()];
-        int lastId = shortestDestId;
-        for (int i = height()-1; i > -1 ; i--) {
-            int x = getX(lastId);
-            int y = getY(lastId);
-            result[i] = x;
-            lastId = edgeTo[x][y];
-        }
-        return result;
     }
 
     // remove horizontal seam from current picture
